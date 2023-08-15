79c1f13d29e39b8a3625db7df451732788480d1b
teganwilliams <s2087047@ed.ac.uk>
2023-08-15 10:51:43 +0100

    new graphs and changes

--- InitialGraphs.R
+++ InitialGraphs.R
@@ -27,37 +27,161 @@ obs_data <- read.csv("~/Desktop/Dissertation/Data/DE-Hai-2000-2020-weekly_timese
 rows_to_delete <- c(3, 7, 10)
 my_data <- my_data[-rows_to_delete, ]
   
-# Create new collumn for full dates 
+# Create new column for full dates 
 rows_per_year <- 52
 met_data$year <- rep(2000:2020, each = rows_per_year)
 met_data$full_date <- as.Date(paste0(met_data$year, "-", met_data$doy), format = "%Y-%j")
 
 # Plot Air Temperature over Time
-my_time_series <- ts(met_data$airt_C, frequency = 52)
-plot(my_time_series, xlab = "Time", ylab = "Air Temperature [C]")
-
-ggplot(data = met_data, aes(x = full_date, y = airt_C)) +
-  geom_line() +
-  labs(x = "Time [year]", y = "Air Temperature [°C]", title = "Air Temperature variations between 2000 and 2020")+
-  #theme_bw() +
-  theme(panel.background = element_blank())
 
 ggplot(met_data, aes(x = full_date, y = airt_C)) +
-      geom_line(colour="blue2") +
-      #geom_smooth(colour = "green3") +
-      theme(legend.position = "bottom", panel.background = element_blank(), axis.line = element_line(colour = "black")) + 
-      labs(title="Temperature trends") + 
-      theme(plot.title=element_text(size=13, hjust=0.5)) + # Title size and position
+      geom_line(colour="red2") +
+      theme(legend.position = "bottom", panel.background = element_blank(), axis.line = element_line(colour = "black"), 
+            plot.title=element_text(size=13, hjust=0.5)) + # Title size and position
+      labs(title="Temperature trends 2000-2020") + 
       xlab("Time [year]") + 
       ylab("Air Temperature [°C]")
 
 # Filter data for the years 2010 to 2020
-filtered_data <- met_data[met_data$year >= 2010 & met_data$year <= 2020, ]
-my_time_series <- ts(filtered_data$airt_C, frequency = 52)
+met2010to2020 <- met_data[met_data$year >= 2010 & met_data$year <= 2020, ]
 
-# Plot the time series
-ggplot(data = filtered_data, aes(x = full_date, y = airt_C)) +
-  geom_line() +
-  labs(x = "Date", y = "Value", title = "Time Series Plot (2010-2020)")
+ggplot(met2010to2020, aes(x = full_date, y = airt_C)) +
+  geom_line(colour="red2") +
+  theme(legend.position = "bottom", panel.background = element_blank(), axis.line = element_line(colour = "black")) + 
+  labs(title="Temperature trends 2010-2020") + 
+  theme(plot.title=element_text(size=13, hjust=0.5)) + # Title size and position
+  xlab("Time [year]") + 
+  ylab("Air Temperature [°C]")
+
+# Now filter for even less years
+met2019 <- met_data[met_data$year >= 2019 & met_data$year <= 2019, ]
+met2018to2020 <- met_data[met_data$year >= 2018 & met_data$year <= 2020, ]
+
+ggplot(met2018to2020, aes(x = full_date, y = airt_C)) +
+  geom_line(colour="red2") +
+  theme(legend.position = "bottom", panel.background = element_blank(), axis.line = element_line(colour = "black")) + 
+  labs(title="Temperature trends 2018-2020") + 
+  theme(plot.title=element_text(size=13, hjust=0.5)) + # Title size and position
+  xlab("Time [year]") + 
+  ylab("Air Temperature [°C]")
+
+# Now let's look at Precipitation over Time
+
+ggplot(met_data, aes(x = full_date, y = precip_kgm2s)) +
+  geom_line(colour="blue2") +
+  theme(legend.position = "bottom", panel.background = element_blank(), axis.line = element_line(colour = "black"), 
+        plot.title=element_text(size=13, hjust=0.5)) + # Title size and position
+  labs(title="Temperature trends 2000-2020") + 
+  xlab("Time [year]") + 
+  ylab("Precipitation [kg/m^2/s]")
+
+# Now the same but at different timescales
+ggplot(met2010to2020, aes(x = full_date, y = precip_kgm2s)) +
+  geom_line(colour="blue2") +
+  theme(legend.position = "bottom", panel.background = element_blank(), axis.line = element_line(colour = "black"), 
+        plot.title=element_text(size=13, hjust=0.5)) + # Title size and position
+  labs(title="Temperature trends 2010-2020") + 
+  xlab("Time [year]") + 
+  ylab("Precipitation [kg/m^2/s]")
+
+ggplot(met2018to2020, aes(x = full_date, y = precip_kgm2s)) +
+  geom_line(colour="blue2") +
+  theme(legend.position = "bottom", panel.background = element_blank(), axis.line = element_line(colour = "black"), 
+        plot.title=element_text(size=13, hjust=0.5)) + # Title size and position
+  labs(title="Temperature trends 2018-2020") + 
+  xlab("Time [year]") + 
+  ylab("Precipitation [kg/m^2/s]")
+
+ggplot(met2019, aes(x = full_date, y = precip_kgm2s)) +
+  geom_line(colour="blue2") +
+  theme(legend.position = "bottom", panel.background = element_blank(), axis.line = element_line(colour = "black"), 
+        plot.title=element_text(size=13, hjust=0.5)) + # Title size and position
+  labs(title="Temperature trends 2019") + 
+  xlab("Time [year]") + 
+  ylab("Precipitation [kg/m^2/s]")
+
+
+# Now lets look at the observation data
+head(obs_data) 
+summary(obs_data) 
+summary(obs_data$doy) 
+str(obs_data)
+glimpse(obs_data)
+
+# Now create a full date column for this table too
+rows_per_year <- 52
+obs_data$year <- rep(2000:2020, each = rows_per_year)
+obs_data$full_date <- as.Date(paste0(obs_data$year, "-", obs_data$doy), format = "%Y-%j")
+
+# Replace -9999e3 with NA in the GPP column
+obs_data$GPP_gCm2day[obs_data$GPP_gCm2day == -9.999000e+03] <- NA
+obs_data$NEE_gCm2day[obs_data$NEE_gCm2day == -9.999000e+03] <- NA
+obs_data$Reco_gCm2day[obs_data$Reco_gCm2day == -9.999000e+03] <- NA
+
+
+# Plot GPP over time
+ggplot(obs_data, aes(x = full_date, y = GPP_gCm2day)) +
+  geom_line(colour="green3") +
+  theme(legend.position = "bottom", panel.background = element_blank(), axis.line = element_line(colour = "black"), 
+        plot.title=element_text(size=13, hjust=0.5)) + # Title size and position
+  labs(title="GPP trends 2000-2020") + 
+  xlab("Time [year]") + 
+  ylab("GPP [gC/m^2/day]")
+
+# Plot GPP against Temperature
+# first combine the datasets
+all_data <- cbind(met_data, obs_data$GPP_gCm2day, obs_data$Reco_gCm2day, obs_data$NEE_gCm2day)
+View(all_data)
+
+y_limits <- c(0, 16)
+
+ggplot(all_data, aes(x = airt_C, y = obs_data$GPP_gCm2day)) +
+  geom_point(colour="green3") +
+  geom_smooth(method = lm, colour = "darkgreen") +
+  theme(legend.position = "bottom", panel.background = element_blank(), axis.line = element_line(colour = "black"), 
+        plot.title=element_text(size=13, hjust=0.5)) + # Title size and position
+  coord_cartesian(ylim = y_limits) + 
+  labs(title="GPP against Air Temperature") + 
+  xlab("Temperature [°C]") + 
+  ylab("GPP [gC/m^2/day]")
+
+# GPP against Precipitation
+
+ggplot(all_data, aes(x = precip_kgm2s, y = obs_data$GPP_gCm2day)) +
+  geom_point(colour="green3") +
+  theme(legend.position = "bottom", panel.background = element_blank(), axis.line = element_line(colour = "black"), 
+        plot.title=element_text(size=13, hjust=0.5)) + # Title size and position
+  labs(title="GPP against precipitation") + 
+  xlab("Precipitation [kg/m^2/s]") + 
+  ylab("GPP [gC/m^2/day]")
+
+# Now Reco
+ggplot(all_data, aes(x = airt_C, y = obs_data$Reco_gCm2day)) +
+  geom_point(colour="orange") +
+  geom_smooth(method = lm, colour = "red4") +
+  theme(legend.position = "bottom", panel.background = element_blank(), axis.line = element_line(colour = "black"), 
+        plot.title=element_text(size=13, hjust=0.5)) + # Title size and position
+  labs(title="Reco against Air Temperature") + 
+  xlab("Temperature [°C]") + 
+  ylab("Reco [gC/m^2/day]")
+
+
+ggplot(all_data, aes(x = precip_kgm2s, y = obs_data$Reco_gCm2day)) +
+  geom_point(colour="orange") +
+  theme(legend.position = "bottom", panel.background = element_blank(), axis.line = element_line(colour = "black"), 
+        plot.title=element_text(size=13, hjust=0.5)) + # Title size and position
+  labs(title="Reco against Precipitation") + 
+  xlab("Precipitation [kg/m2/s]") + 
+  ylab("Reco [gC/m^2/day]")
+
+
+
+
+
+
+
+
+
+
   
   
