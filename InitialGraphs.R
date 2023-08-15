575c67cf050b89d036c2034ff8f6d4dec7cd5ac6
teganwilliams <s2087047@ed.ac.uk>
2023-08-15 09:39:52 +0100

    changes to plot

--- InitialGraphs.R
+++ InitialGraphs.R
@@ -17,7 +17,7 @@ met_data <- read.csv("~/Desktop/Dissertation/Data/DE-Hai-2000-2020-weekly_timese
 obs_data <- read.csv("~/Desktop/Dissertation/Data/DE-Hai-2000-2020-weekly_timeseries_obs.csv", header = TRUE)
 
 # Explore data
-  head(met_data) # Gives first few variables (6 first rows here) e.g. to check data imported OK, get familiar with the variables 
+  head(met_data) # Gives first few variables (6 first rows) e.g. to check data imported OK, get familiar with the variables 
   summary(met_data) # Gives an overall summary of our dataset 
   summary(met_data$doy) # Gives length of the "day of year" column and variable type 
   str(met_data) # Compactly displays the structure of the dataset (type of variable e.g., character, logistic, numeric etc) 
@@ -32,25 +32,24 @@ rows_per_year <- 52
 met_data$year <- rep(2000:2020, each = rows_per_year)
 met_data$full_date <- as.Date(paste0(met_data$year, "-", met_data$doy), format = "%Y-%j")
 
-
 # Plot Air Temperature over Time
-
-my_time_series <- ts(met_data$airt_C, frequency = 52) 
-
-dev.off()
+my_time_series <- ts(met_data$airt_C, frequency = 52)
+plot(my_time_series, xlab = "Time", ylab = "Air Temperature [C]")
 
 ggplot(data = met_data, aes(x = full_date, y = airt_C)) +
   geom_line() +
-  labs(x = "Date", y = "Air Temperature [C]", title = "Temperature over time")
+  labs(x = "Time [year]", y = "Air Temperature [°C]", title = "Air Temperature variations between 2000 and 2020")+
+  #theme_bw() +
+  theme(panel.background = element_blank())
 
 ggplot(met_data, aes(x = full_date, y = airt_C)) +
-      geom_line(colour="green3") +
+      geom_line(colour="blue2") +
       #geom_smooth(colour = "green3") +
-      theme(legend.position = "bottom") + # Legend position 
+      theme(legend.position = "bottom", panel.background = element_blank(), axis.line = element_line(colour = "black")) + 
       labs(title="Temperature trends") + 
-      theme(plot.title=element_text(size=15, hjust=0.5)) + # Title size and position
-      xlab("Time (day of year)") + 
-      ylab("Air Temperature [degrees C]")
+      theme(plot.title=element_text(size=13, hjust=0.5)) + # Title size and position
+      xlab("Time [year]") + 
+      ylab("Air Temperature [°C]")
 
 # Filter data for the years 2010 to 2020
 filtered_data <- met_data[met_data$year >= 2010 & met_data$year <= 2020, ]
