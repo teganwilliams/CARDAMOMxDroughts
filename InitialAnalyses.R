c911a5fa85dc2b0a6fb4b36525668be2a75fb97d
teganwilliams <s2087047@ed.ac.uk>
2024-01-18 14:54:35 +0000

    more edits!

--- InitialAnalyses.R
+++ InitialAnalyses.R
@@ -27,8 +27,49 @@ met_data <- met_data %>%
 
 #### Climate trends and anomalies ####
 
-# Create dataframe for climate averages 
+# Creating seasonal climate averages
+met_data$full_date <- as.Date(met_data$full_date)
+met_data$month <- month(met_data$full_date, label = TRUE)
+seasonal_data <- met_data %>%
+  group_by(year, season = case_when(
+    month %in% c('Dec', 'Jan', 'Feb') ~ 'Winter',
+    month %in% c('Mar', 'Apr', 'May') ~ 'Spring',
+    month %in% c('Jun', 'Jul', 'Aug') ~ 'Summer',
+    month %in% c('Sep', 'Oct', 'Nov') ~ 'Autumn')) %>%
+  summarise(avg_temp = mean(airt_C))
 
+ggplot(seasonal_data, aes(x=year, y = avg_temp, colour = season)) +
+  geom_line() +
+  geom_point() +
+  labs(x = 'Year', y ='Average Temperature', title = 'Seasonal Climate Averages')+
+  scale_color_manual(values = c('Winter'='blue', 'Spring'='green', 'Summer'='red', 'Autumn'='orange'))+
+  theme(legend.position = "right", panel.background = element_blank(), axis.line = element_line(colour = "black"))
+
+# Create dataframe for climate averages (doy, month, temp, precip, sm)
+
+climate_averages <- cbind(mean_month_temp, mean_month_precip)
+column_index_to_delete <- 3
+climate_averages <- climate_averages[,-column_index_to_delete]
+avg_monthly_MaxT <- met_data %>%
+  group_by(year, month) %>%
+  summarise(monthlyMaxT = mean(maxt_C, na.rm = TRUE))
+mean_month_MaxT <- avg_monthly_MaxT %>%
+  group_by(month) %>%
+  summarise(monthlyMaxT = mean(monthlyMaxT, na.rm = TRUE))  
+climate_averages <- cbind(climate_averages, mean_month_maxT)
+column_index_to_delete2 <- 4
+climate_averages <- climate_averages[,-column_index_to_delete2]
+climate_data_long <- gather(climate_averages, key = "temperature_type", value = "temperature", -month, -monthlyPrecip)
+
+
+monthly_norms <- 
+
+# Plot climate norms against anomalies 
+
+T_anomaly_plot <- ggplot() +
+  geom_line(data = met_data_with_avg_temp, aes(x = month, y = monthlyT, group = 1), colour = "red", alpha = 0.5) +
+  geom_line(data = climate_averages, aes(x = month, y = monthlyT), colour = "black", size = 1) 
+              
 
 
 #### Analysis of temperature extremes vs flux data ####
