0ee7c38d7a0647d6961e364d8196dc8f9c63356d
Tegan <114161051+teganwilliams@users.noreply.github.com>
2024-01-23 14:42:33 +0000

    Create ClimateTrends.R

---
+++ R.code/ClimateTrends.R
@@ -0,0 +1,5 @@
+### Climate Trends and Anomalies
+## By Tegan Williams January 2024
+## Data for Göttingen weather station, supplied by Deutscher Wetterdienst
+
+#### Step 1. Calculating daily averages for 1990 to 2020 (30 year trends) ####
