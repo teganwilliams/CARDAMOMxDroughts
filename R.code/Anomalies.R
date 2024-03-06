5535ec7fac409fb87d0185921565f3a3398e51e5
teganwilliams <s2087047@ed.ac.uk>
2024-03-06 16:44:59 +0000

    ++

--- R.code/Anomalies.R
+++ R.code/Anomalies.R
@@ -13,6 +13,7 @@ library(ggplot2)
 met_data <- read.csv("Data/DE-Hai-2000-2020-weekly_timeseries_met.csv", header = TRUE)
 obs_data <- read.csv("Data/DE-Hai-2000-2020-weekly_timeseries_obs.csv", header = TRUE)
 sm_data <- read.csv("Data/DE-Hai-2000-2020-weekly_timeseries_metSM.csv", header= TRUE)
+climate_data <- read_csv("Data/DE-Hai_FLUXNET2015_DD_1989-2020_met.csv")
 
 # Delete 2021 data from Met data set since only NA values (-9999)
 rows_to_delete <- c(1093:1144)
@@ -90,7 +91,6 @@ ggplot(met_data_with_avg_temp_05, aes(x = doy, y = airt_C, colour = as.factor(ye
   theme(legend.position = "right", panel.background = element_blank(), axis.line = element_line(colour = "black"))
 
 
-
 # Precipitation over Time
 ggplot(met2000to2005, aes(x = full_date, y = precip_kgm2s)) +
   geom_line(colour="blue2") +
@@ -99,6 +99,133 @@ ggplot(met2000to2005, aes(x = full_date, y = precip_kgm2s)) +
   labs(title="Precipitation trends 2000-2005") + 
   xlab("Time [year]") + 
   ylab("Precipitation [kg/m^2/s]")
+
+
+#### Plotting Temperature Anomalies ####
+
+# Reference period 1989-2020
+climate_data <- climate_data %>%
+  mutate(TIMESTAMP = as.character(TIMESTAMP),
+         year = year(as.Date(TIMESTAMP, format = "%Y%m%d")),
+         month = month(as.Date(TIMESTAMP, format = "%Y%m%d")),
+         doy = yday(as.Date(TIMESTAMP, format = "%Y%m%d")))
+
+daily_averages <- climate_data %>%
+  group_by(doy) %>%
+  summarize(AverageTemperature = mean(MeanT, na.rm = TRUE))
+
+datafor2003 <- climate_data %>%
+  filter(year == 2003)
+datafor2018 <- climate_data %>%
+  filter(year == 2018)
+dataforboth <- climate_data %>%
+  filter(year %in% c(2003, 2018))
+
+temp_reference_period <- climate_data %>%
+  filter(year >= 1989 & year <= 2020) %>%
+  group_by(doy) %>%
+  summarize(tAverage = mean(MeanT, na.rm = TRUE))
+
+# Merge climate data with reference period
+temp_merged_data <- merge(climate_data, temp_reference_period, by = "doy", all.x = TRUE)
+
+# Calculate temperature anomalies
+temp_merged_data$tempAnomaly <- temp_merged_data$MeanT - temp_merged_data$tAverage
+
+# Calculate the 90th percentile of temperature anomalies
+temp_percentile_90 <- quantile(temp_merged_data$tempAnomaly, 0.9, na.rm = TRUE)
+temp_percentile_0 <- quantile(temp_merged_data$tempAnomaly, 0, na.rm = TRUE)
+
+# Filter only values in the chosen percentile
+temp_anomaly_data <- temp_merged_data %>%
+  filter(tempAnomaly > temp_percentile_0)
+temp_anomaly_data90 <- temp_merged_data %>%
+  filter(tempAnomaly > temp_percentile_90)
+
+min(temp_anomaly_data90$tempAnomaly)
+max(temp_anomaly_data90$tempAnomaly)
+
+temp_anomaly_data$year <- as.factor(temp_anomaly_data$year)
+temp_anomaly_data$doy <- as.factor(temp_anomaly_data$doy)
+
+temp_anomaly_data_filtered <- temp_anomaly_data[c("year", "doy", "tAverage", "tempAnomaly")]
+temp_anomaly_data_filtered$doy <- as.numeric(temp_anomaly_data_filtered$doy)
+
+# Plotting Temperature Anomalies over Time
+
+# 1) using daily values
+ggplot(temp_anomaly_data_filtered, aes(x = doy, y = tempAnomaly, colour = year)) +
+  geom_line() +
+  geom_hline(yintercept = 0, linetype = "dashed", colour = "black") +
+  geom_hline(yintercept = 4.63, linetype = "dashed", colour = "darkorange") +
+  geom_text(aes(x = 235, y = -11, label = "90th Percentile"), colour = "darkorange") + 
+  geom_text(aes(x = 242, y = 1, label = "Norm"), colour = "black") + 
+  labs(title = "Summer Temperature Anomalies compared to 30 year average",
+       x = "",
+       y = "Daily Temp Anomaly (degrees C)") +
+  scale_colour_manual(values = c("grey","grey","grey","grey","grey","grey","grey","grey","grey","grey","grey","grey","grey","grey","deeppink","grey", "grey","grey","grey","grey","grey","grey","grey","grey","grey","grey","grey", "grey","grey", "red2", "grey", "grey")) +
+  theme(legend.position = "bottom", panel.background = element_blank(), axis.line = element_line(colour = "black"), 
+        plot.title=element_text(size=13, hjust=0.5)) +
+  scale_x_continuous(breaks = c(154, 182, 213, 242), 
+                     labels = c("Jun", "Jul", "Aug", "Sep"),
+                     expand = c(0, 0),
+                     limits = c(152, 245)) +
+  scale_y_continuous(expand = c(0, 0),
+                     limits = c(0,11))
+
+
+# 2) using weekly temperature means to calculate anomalies
+weekly_average_temp_data <- climate_data %>%
+  group_by(week = ceiling(as.numeric(doy)/7)) %>%
+  summarise(AverageWeeklyMeanT = mean(MeanT, na.rm = TRUE))
+
+weekly_temp_data <- climate_data %>%
+  group_by(year, week = ceiling(as.numeric(doy)/7)) %>%
+  summarise(WeeklyMeanT = mean(MeanT, na.rm = TRUE))
+
+# now find the anomaly values
+weekly_temp_merged_data <- merge(weekly_average_temp_data, weekly_temp_data, by = "week", all.x = TRUE)
+weekly_temp_merged_data$tempAnomaly <- weekly_temp_merged_data$WeeklyMeanT - weekly_temp_merged_data$AverageWeeklyMeanT
+weekly_temp_percentile_90 <- quantile(weekly_temp_merged_data$tempAnomaly, 0.9, na.rm = TRUE)
+weekly_temp_percentile_0 <- quantile(weekly_temp_merged_data$tempAnomaly, 0, na.rm = TRUE)
+
+weekly_temp_anomaly_data <- weekly_temp_merged_data %>%
+  filter(tempAnomaly > weekly_temp_percentile_0)
+weekly_temp_anomaly_data90 <- weekly_temp_merged_data %>%
+  filter(tempAnomaly > weekly_temp_percentile_90)
+
+min(weekly_temp_anomaly_data90$tempAnomaly)
+max(weekly_temp_anomaly_data90$tempAnomaly)
+
+temp_anomaly_data$year <- as.factor(temp_anomaly_data$year)
+temp_anomaly_data$doy <- as.factor(temp_anomaly_data$doy)
+
+weekly_temp_anomaly_data$year <- as.factor(weekly_temp_anomaly_data$year)
+weekly_temp_anomaly_data$week <- as.numeric(weekly_temp_anomaly_data$week)
+
+ggplot(weekly_temp_anomaly_data, aes(x = week, y = tempAnomaly, colour = year)) +
+  # geom_rect(aes(xmin = 119, xmax = 245, 
+  #          ymin = -15, ymax = -6.98), 
+  #    fill = "orange", alpha = 0.5) +
+  geom_line() +
+  geom_hline(yintercept = 0, linetype = "dashed", colour = "black") +
+  geom_hline(yintercept = 3.63, linetype = "dashed", colour = "darkorange") +
+  geom_text(aes(x = 242, y = 1, label = "Norm"), colour = "black") + 
+  geom_text(aes(x = 235, y = -11, label = "90th Percentile"), colour = "darkorange") + 
+  labs(title = "Summer Weekly Temperature Anomalies compared to 30 year average",
+       x = "",
+       y = "Temperature Anomaly (degrees Celsius)") +
+  scale_colour_manual(values = c("grey","grey","grey","grey","grey", "blue","grey","grey","grey","grey","grey","grey","grey","grey","deeppink","grey","grey","grey","grey","grey","grey","grey","grey","grey", "grey", "grey","grey","grey","grey","red2", "grey", "grey")) +
+  theme(legend.position = "bottom", panel.background = element_blank(), axis.line = element_line(colour = "black"), 
+        plot.title=element_text(size=13, hjust=0.5)) +
+  scale_x_continuous(breaks = c(18, 22, 27, 32, 36), 
+                     labels = c("May", "Jun", "Jul", "Aug", "Sep"),
+                     expand = c(0, 0),
+                     limits = c(18, 36)) +
+  scale_y_continuous(expand = c(0, 0),
+                     limits = c(-5,8))
+
+View(weekly_temp_anomaly_data)
 
 
 
