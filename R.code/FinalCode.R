3a3de54b3f8632f04056d23f876f9bf4b2a7248c
Tegan Williams <s2087047@baltic10.geos.ed.ac.uk>
2024-03-27 13:52:45 +0000

    few more changes

--- R.code/FinalCode.R
+++ R.code/FinalCode.R
@@ -172,7 +172,7 @@ print(gpprelative_rmse2000)
 # Plot the relationships
 gppcorrelation2000 <- ggplot(data2000, aes(x = mod_gpp, y = obs_gpp)) +
   geom_point(colour = "#5A00C75E") +
-  labs(x = "Modelled GPP (gC/m²/day)", y = "Observed GPP (gC/m²/day)") +
+  labs(x = "Modelled GPP₂₀₀₃ (gC/m²/day)", y = "Observed GPP₂₀₀₃ (gC/m²/day)") +
   geom_abline(intercept = 0, slope = 1, color = "#5D1CAD", size = 0.6) +
   # geom_abline(intercept = 0, slope = max(data2000$obs_gpp, na.rm = TRUE) / max(data2000$mod_gpp, na.rm = TRUE), linetype = "dashed", color = "black") +
   geom_text(aes(x = 0, 
@@ -192,7 +192,7 @@ plot(gppcorrelation2000)
 
 laicorrelation2000 <- ggplot(data2000, aes(x = mod_lai, y = obs_lai)) +
   geom_point(colour = "#059C0093") +
-  labs(x = "Modelled LAI (m² / m²)", y = "Observed LAI (m² / m²)") +
+  labs(x = "Modelled LAI₂₀₀₃ (m² / m²)", y = "Observed LAI₂₀₀₃ (m² / m²)") +
   geom_abline(intercept = 0, slope = 1, color = "darkgreen", size = 0.6) +
   # geom_abline(intercept = 0, slope = max(data2000$obs_lai, na.rm = TRUE) / max(data2000$mod_lai, na.rm = TRUE), linetype = "dashed", color = "black") +
   geom_text(aes(x = 0, 
@@ -211,9 +211,9 @@ laicorrelation2000 <- ggplot(data2000, aes(x = mod_lai, y = obs_lai)) +
 plot(laicorrelation2000)
 
 recocorrelation2000 <- ggplot(data2000, aes(x = mod_reco, y = obs_reco)) +
-  geom_point(colour = "orange") +
-  labs(x = "Modelled Reco (gC/m²/day)", y = "Observed Reco (gC/m²/day)") +
-  geom_abline(intercept = 0, slope = 1, color = "darkorange2", size = 0.6) +
+  geom_point(colour = "#eb1d129b") +
+  labs(x = "Modelled Reco₂₀₀₃ (gC/m²/day)", y = "Observed Reco₂₀₀₃ (gC/m²/day)") +
+  geom_abline(intercept = 0, slope = 1, color = "#ba0d04e5", size = 0.6) +
   # geom_abline(intercept = 0, slope = max(data2000$obs_reco, na.rm = TRUE) / max(data2000$mod_reco, na.rm = TRUE), linetype = "dashed", color = "black") +
   geom_text(aes(x = 0, 
                 y = 6.5), 
@@ -221,7 +221,7 @@ recocorrelation2000 <- ggplot(data2000, aes(x = mod_reco, y = obs_reco)) +
             hjust = 0, vjust = 1,
             size = 4, 
             fontface = "bold", 
-            colour = "darkorange2") +
+            colour = "#ba0d04e5") +
   theme(legend.position = "bottom", panel.background = element_blank(), axis.line = element_line(colour = "black"), 
         axis.title = element_text(size=11),
         axis.text = element_text(size=9)) +
@@ -253,7 +253,7 @@ print(gpprelative_rmse2015)
 # Plot the relationships
 gppcorrelation2015 <- ggplot(data2015, aes(x = mod_gpp, y = obs_gpp)) +
   geom_point(colour = "#5A00C75E") +
-  labs(x = "Modelled GPP (gC/m²/day)", y = "Observed GPP (gC/m²/day)") +
+  labs(x = "Modelled GPP₂₀₁₈ (gC/m²/day)", y = "2015-2020 Observed GPP (gC/m²/day)") +
   geom_abline(intercept = 0, slope = 1, color = "#5D1CAD", size = 0.6) +
   # geom_abline(intercept = 0, slope = max(data2015$obs_gpp, na.rm = TRUE) / max(data2015$mod_gpp, na.rm = TRUE), linetype = "dashed", color = "black") +
   geom_text(aes(x = 0, 
@@ -292,9 +292,9 @@ laicorrelation2015 <- ggplot(data2015, aes(x = mod_lai, y = obs_lai)) +
 plot(laicorrelation2015)
 
 recocorrelation2015 <- ggplot(data2015, aes(x = mod_reco, y = obs_reco)) +
-  geom_point(colour = "orange") +
+  geom_point(colour = "#eb1d129b") +
   labs(x = "Modelled Reco (gC/m²/day)", y = "Observed Reco (gC/m²/day)") +
-  geom_abline(intercept = 0, slope = 1, color = "darkorange2", size = 0.6) +
+  geom_abline(intercept = 0, slope = 1, color = "#ba0d04e5", size = 0.6) +
   # geom_abline(intercept = 0, slope = max(data2015$obs_reco, na.rm = TRUE) / max(data2015$mod_reco, na.rm = TRUE), linetype = "dashed", color = "black") +
   geom_text(aes(x = 0, 
                 y = 6.5), 
@@ -302,7 +302,7 @@ recocorrelation2015 <- ggplot(data2015, aes(x = mod_reco, y = obs_reco)) +
             hjust = 0, vjust = 1,
             size = 4, 
             fontface = "bold", 
-            colour = "darkorange2") +
+            colour = "#ba0d04e5") +
   theme(legend.position = "bottom", panel.background = element_blank(), axis.line = element_line(colour = "black"), 
         axis.title = element_text(size=11),
         axis.text = element_text(size=9)) +
