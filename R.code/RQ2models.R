aa70eb08586818c17433cc7d7086dd9b054b4677
teganwilliams <s2087047@ed.ac.uk>
2024-04-08 00:23:29 +0100

    anova

--- R.code/RQ2models.R
+++ R.code/RQ2models.R
@@ -1,3 +1,215 @@
 #### R script for MODELS in RQ2 ####
 
-# Load data 
+# Load libraries
+library(dplyr)
+library(tidyverse)
+library(ggplot2) 
+
+# Load Data
+zscores <- read.csv("Data/fullanomalies.csv", header = TRUE)
+
+# Create subset dataframes for drought vs non drought
+
+summer_zscores <- zscores %>%
+  filter(week >= 22 & week <= 37)
+
+zscores <- zscores %>%
+  mutate(condition = ifelse(year %in% c(2003, 2018), "drought", "normal"))
+
+zscores$year_group <- ifelse(zscores$year %in% c(2000, 2001, 2002, 2004, 2005, 2015, 2016, 2017, 2019, 2020), "normal",
+                             # ifelse(drivers$year %in% c(2015, 2016, 2017, 2019, 2020), "2015-2020",
+                             ifelse(zscores$year %in% c(2003), "2003", 
+                                    ifelse(zscores$year %in% c(2018), "2018", NA)))
+
+nondrought <- zscores %>%
+  filter(!(year %in% c(2003, 2018)))
+
+nondrought <- nondrought %>%
+  group_by(doy) %>%
+  mutate(mean_gpp = mean(gpp_z_scores, na.rm = TRUE),
+         mean_maxT = mean(temp_z_scores, na.rm = TRUE),
+         mean_sm1 = mean(sm1_z_scores, na.rm = TRUE),
+         mean_sm2 = mean(sm2_z_scores, na.rm = TRUE),
+         mean_sm3 = mean(sm3_z_scores, na.rm = TRUE),
+         mean_vpd = mean(vpd_z_scores, na.rm = TRUE),
+         mean_swr = mean(swr_z_scores, na.rm = TRUE)) %>%
+  ungroup()
+
+drought1 <- zscores %>%
+  filter((year %in% c(2003, 2018)))
+
+drought1 <- drought1 %>%
+  group_by(year) %>%
+  mutate(mean_maxT = temp_z_scores, mean_gpp = gpp_z_scores, mean_sm1 = sm1_z_scores, mean_sm2 = sm2_z_scores, mean_sm3 = sm3_z_scores, mean_vpd = vpd_z_scores, mean_swr = swr_z_scores) %>%
+  ungroup()
+
+new <- rbind(drought1, nondrought)
+
+#### Timeseries plots of z-scores ####
+palette_anomalies <- c("#D6A400", "#B80422", "darkgrey")
+
+gpp_plotz <- ggplot(new, aes(x = doy, y = mean_gpp, group = year_group, colour = year_group, linetype = condition)) +
+  geom_line(size = 0.8) +
+  labs(title = "",
+       x = "Summer months",
+       y = "GPP (gC/m²/day)",
+       colour = "Year:") +
+  scale_colour_manual(values = palette_anomalies) +
+  #scale_shape_manual() + 
+  theme(legend.position = "none", panel.background = element_blank(), axis.line = element_line(colour = "black"), 
+        plot.title = element_text(size=12, hjust=0.5),
+        axis.title = element_text(size=11),
+        axis.text = element_text(size=9),
+        legend.title = element_text(size = 11, face = "bold", ),
+        legend.text = element_text(size = 11),
+        plot.margin = margin(1, 1, 1, 1, "cm")) +
+  scale_x_continuous(breaks = c(126, 154, 182, 217, 252), 
+                     labels = c("May", "Jun", "Jul", "Aug", "Sep"))
+
+plot(gpp_plotz)
+
+maxT_plotz <- ggplot(new, aes(x = doy, y = mean_maxT, group = year_group, colour = year_group, linetype = condition)) +
+  geom_line(size = 0.8) +
+  labs(title = "",
+       x = "Time (months)",
+       y = "Max temperature (°C)",
+       colour = "Year:") +
+  scale_colour_manual(values = palette_anomalies) +
+  #scale_shape_manual() + 
+  theme(legend.position = "none", panel.background = element_blank(), axis.line = element_line(colour = "black"), 
+        plot.title = element_text(size=12, hjust=0.5),
+        axis.title = element_text(size=11),
+        axis.text = element_text(size=9),
+        legend.title = element_text(size = 11, face = "bold", ),
+        legend.text = element_text(size = 11),
+        plot.margin = margin(1, 1, 1, 1, "cm")) +
+  scale_x_continuous(breaks = c(126, 154, 182, 217, 252), 
+                     labels = c("May", "Jun", "Jul", "Aug", "Sep"))
+
+plot(maxT_plotz)
+
+sm1_plotz <- ggplot(new, aes(x = doy, y = mean_sm1, group = year_group, colour = year_group, linetype = condition)) +
+  geom_line(size = 0.8) +
+  labs(title = "",
+       x = "Time (month)",
+       y = "Soil moisture at depth 2 (X)",
+       colour = "Year:") +
+  scale_colour_manual(values = palette_anomalies) +
+  #scale_shape_manual() + 
+  theme(legend.position = "none", panel.background = element_blank(), axis.line = element_line(colour = "black"), 
+        plot.title = element_text(size=12, hjust=0.5),
+        axis.title = element_text(size=11),
+        axis.text = element_text(size=9),
+        legend.title = element_text(size = 11, face = "bold", ),
+        legend.text = element_text(size = 11),
+        plot.margin = margin(1, 1, 1, 1, "cm")) +
+  scale_x_continuous(breaks = c(126, 154, 182, 217, 252), 
+                     labels = c("May", "Jun", "Jul", "Aug", "Sep"))
+
+plot(sm1_plotz)
+
+
+
+sm2_plotz <- ggplot(new, aes(x = doy, y = mean_sm2, group = year_group, colour = year_group, linetype = condition)) +
+  geom_line(size = 0.8) +
+  labs(title = "",
+       x = "Time (month)",
+       y = "Soil moisture at depth 2 (X)",
+       colour = "Year:") +
+  scale_colour_manual(values = palette_anomalies) +
+  #scale_shape_manual() + 
+  theme(legend.position = "none", panel.background = element_blank(), axis.line = element_line(colour = "black"), 
+        plot.title = element_text(size=12, hjust=0.5),
+        axis.title = element_text(size=11),
+        axis.text = element_text(size=9),
+        legend.title = element_text(size = 11, face = "bold", ),
+        legend.text = element_text(size = 11),
+        plot.margin = margin(1, 1, 1, 1, "cm")) +
+  scale_x_continuous(breaks = c(126, 154, 182, 217, 252), 
+                     labels = c("May", "Jun", "Jul", "Aug", "Sep"))
+
+plot(sm2_plotz)
+
+sm3_plotz <- ggplot(new, aes(x = doy, y = mean_sm3, group = year_group, colour = year_group, linetype = condition)) +
+  geom_line(size = 0.8) +
+  labs(title = "",
+       x = "Time (month)",
+       y = "Soil moisture at depth 3 (X)",
+       colour = "Year:") +
+  scale_colour_manual(values = palette_anomalies) +
+  #scale_shape_manual() + 
+  theme(legend.position = "none", panel.background = element_blank(), axis.line = element_line(colour = "black"), 
+        plot.title = element_text(size=12, hjust=0.5),
+        axis.title = element_text(size=11),
+        axis.text = element_text(size=9),
+        legend.title = element_text(size = 11, face = "bold", ),
+        legend.text = element_text(size = 11),
+        plot.margin = margin(1, 1, 1, 1, "cm")) +
+  scale_x_continuous(breaks = c(126, 154, 182, 217, 252), 
+                     labels = c("May", "Jun", "Jul", "Aug", "Sep"))
+
+plot(sm3_plotz)
+
+
+vpd_plotz <- ggplot(new, aes(x = doy, y = mean_vpd, group = year_group, colour = year_group, linetype = condition)) +
+  geom_line(size = 0.8) +
+  labs(title = "",
+       x = "Time (month)",
+       y = "VPD (kPa)",
+       colour = "Year:") +
+  scale_colour_manual(values = palette_anomalies) +
+  #scale_shape_manual() + 
+  theme(legend.position = "none", panel.background = element_blank(), axis.line = element_line(colour = "black"), 
+        plot.title = element_text(size=12, hjust=0.5),
+        axis.title = element_text(size=11),
+        axis.text = element_text(size=9),
+        legend.title = element_text(size = 11, face = "bold", ),
+        legend.text = element_text(size = 11),
+        plot.margin = margin(1, 1, 1, 1, "cm")) +
+  scale_x_continuous(breaks = c(126, 154, 182, 217, 252), 
+                     labels = c("May", "Jun", "Jul", "Aug", "Sep"))
+plot(vpd_plotz)
+
+swr_plotz <- ggplot(new, aes(x = doy, y = mean_swr, group = year_group, colour = year_group, linetype = condition)) +
+  geom_line(size = 0.8) +
+  labs(title = "",
+       x = "Time (months)",
+       y = "SWR (MJ/m²/day)",
+       colour = "Year:") +
+  scale_colour_manual(values = palette_anomalies) +
+  #scale_shape_manual() + 
+  theme(legend.position = "none", panel.background = element_blank(), axis.line = element_line(colour = "black"), 
+        plot.title = element_text(size=12, hjust=0.5),
+        axis.title = element_text(size=11),
+        axis.text = element_text(size=9),
+        legend.title = element_text(size = 11, face = "bold", ),
+        legend.text = element_text(size = 11),
+        plot.margin = margin(1, 1, 1, 1, "cm")) +
+  scale_x_continuous(breaks = c(126, 154, 182, 217, 252), 
+                     labels = c("May", "Jun", "Jul", "Aug", "Sep"))
+plot(swr_plotz)
+
+
+
+
+
+
+
+#### Modelling drivers relationship with GPP
+
+# ANOVA
+# Combined ANOVA
+anova_all <- aov(gpp_z_scores ~ temp_z_scores + sm1_z_scores + sm2_z_scores + sm3_z_scores + vpd_z_scores + swr_z_scores, data = drought1)
+summary(anova_all) # no significant effect
+
+anova_maxT <- aov(gpp_z_scores ~ temp_z_scores, data = nondrought)
+anova_sm3 <- aov(mod_gpp ~ sm3, data = drought)
+anova_vpd <- aov(mod_gpp ~ vpd, data = drought)
+anova_swr <- aov(mod_gpp ~ swr, data = drought)
+anova_all <- aov(gpp_z_scores ~ temp_z_scores + sm1_z_scores + sm2_z_scores + sm3_z_scores + vpd_z_scores + swr_z_scores, data = nondrought)
+summary(anova_all)
+summary(anova_maxT)
+
+
+
+
